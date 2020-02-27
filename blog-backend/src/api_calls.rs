90345d33ae129fd3873fd63448ab555e9bf4dc9f
JackStaples <Jackstaplesjcrs@gmail.com>
2020-02-27 12:31:53 -0800

    very simple JSON response now working for /posts endpoint

--- blog-backend/src/api_calls.rs
+++ blog-backend/src/api_calls.rs
@@ -1,15 +1,19 @@
 pub mod open_calls {
-    use crate::parameter_structs::parameter_structs;
+    use crate::parameter_structs::parameter_structs::*;
     use actix_web::{get, web, HttpResponse, Responder};
 
     // Get request that returns a JSON file of post titles and their corresponding id's
     #[get("/posts")]
     async fn get_posts() -> impl Responder {
-        HttpResponse::Ok().body("This will return a bunch of post titles and id's!")
+        let post: PostInformation = PostInformation {
+            title: "This is the title of a test post".to_string(),
+            id: 1,
+        };
+        HttpResponse::Ok().json(post)
     }
 
     #[get("/posts/{postid}")]
-    async fn get_posts_by_id(info: web::Path<parameter_structs::PostId>) -> impl Responder {
+    async fn get_posts_by_id(info: web::Path<PostId>) -> impl Responder {
         HttpResponse::Ok().body(format!(
             "This will return the content of the post with the id: {}",
             info.postid
--- blog-backend/src/parameter_structs.rs
+++ blog-backend/src/parameter_structs.rs
@@ -3,10 +3,16 @@
     to capture parameters for api calls
 */
 pub mod parameter_structs {
-    use serde::Deserialize;
+    use serde::{Deserialize, Serialize};
 
     #[derive(Deserialize)]
     pub struct PostId {
         pub postid: u32,
+    }
+
+    #[derive(Serialize, Deserialize)]
+    pub struct PostInformation {
+        pub title: String,
+        pub id: i32,
     }
 }
