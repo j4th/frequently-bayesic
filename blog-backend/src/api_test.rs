b2e014163d8d8a95240dad1a7d4da9d3497e2390
JackStaples <Jackstaplesjcrs@gmail.com>
2020-02-27 12:44:07 -0800

    test case that checks JSON formattting is corrected, need to review if this is an acceptable way to test

--- blog-backend/src/api_test.rs
+++ blog-backend/src/api_test.rs
@@ -1,5 +1,6 @@
 pub mod api_tests {
     use crate::api_calls::open_calls;
+    use crate::parameter_structs::parameter_structs::PostInformation;
     use actix_web::{test, App};
 
     // Test to check that the /posts endpoint responds
@@ -12,13 +13,22 @@ pub mod api_tests {
         assert!(resp.status().is_success());
     }
 
+    #[actix_rt::test]
+    async fn test_get_posts_responds_with_json() {
+        let mut app = test::init_service(App::new().service(open_calls::get_posts)).await;
+        let req = test::TestRequest::get().uri("/posts").to_request();
+        let resp: Vec<PostInformation> = test::read_response_json(&mut app, req).await;
+
+        // if the PostInformation objects serialize succesfully then we can assume the JSON is as expected
+        assert!(true);
+    }
+
     // Test to check that an invalid endpoint /wrongEndpoint does not respond
     #[actix_rt::test]
     async fn test_get_posts_fails() {
         let mut app = test::init_service(App::new().service(open_calls::get_posts)).await;
         let req = test::TestRequest::get().uri("/wrongEndpoint").to_request();
         let resp = test::call_service(&mut app, req).await;
-
         assert!(!resp.status().is_success());
     }
 
